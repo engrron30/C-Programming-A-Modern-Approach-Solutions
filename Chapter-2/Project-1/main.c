0acbf417ce165c6e88edda8188394a8fa581a10a
engrron30 <ronaldtorresrosete30@gmail.com>
2025-04-25 01:19:52 +0800

    Negate logic to always run the space detector first for less operations

--- Chapter-2/Project-1/main.c
+++ Chapter-2/Project-1/main.c
@@ -9,10 +9,10 @@ int main() {
 	for (i = CHECK_HEIGHT - 1; i >= 0; i--) {
 		for (j = i + CHECK_DIMS_DIFF - 1; j >= 0; j--) {
 			// print * if (i is 1 or 2) and (j is 1 or 2)
-			if ((i == 2 && j == 3) || (i == 1 && j == 1)) {
-				printf("*");
-			} else {
+			if (!((i == 2 && j == 3) || (i == 1 && j == 1))) {
 				printf(" ");
+			} else {
+				printf("*");
 			}
 		}
 		printf("*");
