bf5ecc4d34e03997c2c6934e43f2f06f75b67d21
engrron30 <ronaldtorresrosete30@gmail.com>
2025-04-25 07:13:33 +0800

    Added comment for how to use and stop the program immediately if check dimensions are not satisfied

--- Chapter-2/Project-1/main.c
+++ Chapter-2/Project-1/main.c
@@ -1,10 +1,25 @@
 #include <stdio.h>
 #include <stdbool.h>
 
-/*
- * NOTE:
+/* HOW TO USE:
+ *
+ * 	Check Symbol Dimensions:
+ *
+ *	       CHECK_WIDTH
+ *      	<------>
+ *
+ *		       *   ↑
+ *		      *    |
+ *		     *	   |	CHECK_HEIGHT
+ *		*   *	   |
+ *		 * *	   |
+ *		  *	   ↓
+ *
+ *
  * 	(1) Always use bigger CHECK_WIDTH than CHECK_HEIGHT;
  * 	(2) Difference between width and height must not be equal or less than height
+ *
+ *
 */
 
 #define CHECK_HEIGHT	6
@@ -12,6 +27,12 @@
 #define CHECK_DIMS_DIFF	CHECK_WIDTH - CHECK_HEIGHT
 
 int main() {
+	
+	if (CHECK_DIMS_DIFF <= 0 || CHECK_DIMS_DIFF >= CHECK_HEIGHT) {
+		printf("Error: Check dimensions are invalid./n");
+		return -1;
+	}
+
 	int i, j, dec_for_diff_detect = CHECK_DIMS_DIFF;
 	for (i = CHECK_HEIGHT - 1; i >= 0; i--) {
 		bool diff_found = false;
