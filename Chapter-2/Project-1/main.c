da5e268a6b4e5fad1f96e87dcf57b1fbadb4b149
engrron30 <ronaldtorresrosete30@gmail.com>
2025-04-25 02:20:59 +0800

    Add logic cjeck creation for different width amd height as long as difference between them is (0, height)

--- Chapter-2/Project-1/main.c
+++ Chapter-2/Project-1/main.c
@@ -1,18 +1,29 @@
 #include <stdio.h>
+#include <stdbool.h>
 
-#define CHECK_HEIGHT	6
-#define CHECK_WIDTH	8
+/*
+ * NOTE:
+ * 	(1) Always use bigger CHECK_WIDTH than CHECK_HEIGHT;
+ * 	(2) Difference between width and height must not be equal or less than height
+*/
+
+#define CHECK_HEIGHT	4
+#define CHECK_WIDTH	9
 #define CHECK_DIMS_DIFF	CHECK_WIDTH - CHECK_HEIGHT
 
 int main() {
-	int i, j;
+	int i, j, dec_for_diff_detect = CHECK_DIMS_DIFF;
 	for (i = CHECK_HEIGHT - 1; i >= 0; i--) {
+		bool diff_found = false;
 		for (j = i + CHECK_DIMS_DIFF - 1; j >= 0; j--) {
-			// print * if (i is 1 or 2) and (j is 1 or 2)
-			if (!((i == 2 && j == 3) || (i == 1 && j == 1))) {
+			if (!( (i <= CHECK_DIMS_DIFF) && (j == i + dec_for_diff_detect - 1) && (i != 0) && (!diff_found))) {
 				printf(" ");
 			} else {
-				printf("*");
+				if (!diff_found) {
+					diff_found = true;
+					dec_for_diff_detect -= 1;
+					printf("*");
+				}
 			}
 		}
 		printf("*");
