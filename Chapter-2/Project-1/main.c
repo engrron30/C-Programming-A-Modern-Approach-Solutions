8a0ec22aaf11708cdd5ffa18842e79da5ba21da6
engrron30 <ronaldtorresrosete30@gmail.com>
2025-04-25 01:07:06 +0800

    Working check symbol but with hard-coded logic for iterations

--- Chapter-2/Project-1/main.c
+++ Chapter-2/Project-1/main.c
@@ -1,13 +1,19 @@
 #include <stdio.h>
 
-#define CHECK_SYMBOL_HEIGHT	6
-#define CHECK_SYMBOL_WIDTH	8
+#define CHECK_HEIGHT	6
+#define CHECK_WIDTH	8
+#define CHECK_DIMS_DIFF	CHECK_WIDTH - CHECK_HEIGHT
 
 int main() {
 	int i, j;
-	for (i = CHECK_SYMBOL_HEIGHT; i >= 0; i--) {
-		for (j = i + 2; j >= 0; j--) {
+	for (i = CHECK_HEIGHT - 1; i >= 0; i--) {
+		for (j = i + CHECK_DIMS_DIFF - 1; j >= 0; j--) {
+			// print * if (i is 1 or 2) and (j is 1 or 2)
+			if ((i == 2 && j == 3) || (i == 1 && j == 1)) {
+				printf("*");
+			} else {
 				printf(" ");
+			}
 		}
 		printf("*");
 		printf("\n");
