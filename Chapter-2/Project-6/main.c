b95f5a5a78af5178fb3178f2b6a64d335ea91dfc
engrron30 <ronaldtorresrosete30@gmail.com>
2025-04-26 11:00:06 +0800

    Add solution for Project-6

--- Chapter-2/Project-6/main.c
+++ Chapter-2/Project-6/main.c
@@ -8,11 +8,8 @@ int main() {
 		return 1;
 	}
 
-	long double y =   (3*x*x*x*x*x)
-			+ (2*x*x*x*x)
-			- (5*x*x*x)
-			- (x*x)
-			+ (7*x) - 6;
+	// Using Horner's method for efficient polynomial evaluation
+	long double y = (((((3*x + 2)*x - 5)*x - 1)*x + 7)*x - 6);
 	printf("3x^5 + 2x^4 - 5x^3 - x^2 + 7x - 6 = %0.2Lf\n", y);
 
 	return 0;
